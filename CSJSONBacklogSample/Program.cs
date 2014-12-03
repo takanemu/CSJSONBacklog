a4ddd65fb99442bb5904a14fc11e2f6fc92e6641
mtaniuchi <mtaniuchi@gmail.com>
2014-12-03 20:28:27 +0900

    [modified]sample Program

--- CSJSONBacklogSample/Program.cs
+++ CSJSONBacklogSample/Program.cs
@@ -1,11 +1,11 @@
 ﻿/* See the file "LICENSE" for the full license governing this code. */
 
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using CSJSONBacklog.Communicator;
 using CSJSONBacklog.Model.Issues;
+using CSJSONBacklog.Model.Projects;
 
 namespace CSJSONBacklogSample
 {
@@ -16,13 +16,29 @@ namespace CSJSONBacklogSample
             const string spaceName = @"yourSpaceName";// TODO:must change!
             const string apiKey = @"yourApiKey";// TODO:must change!
 
-            // get project list
+            // Dump all project in space
+            var projects = DumpProjects(spaceName, apiKey);
+            
+            // Dump issues in a project
+            var issueCommunicator = new IssueCommunicator(spaceName, apiKey);
+            foreach (var project in projects)
+            {
+                DumpIssues(issueCommunicator, project);
+            }
+        }
+
+        /// <summary>
+        /// Print all project info in space
+        /// </summary>
+        private static IEnumerable<Project> DumpProjects(string spaceName, string apiKey)
+        {
+            // project list
             var projectCommunicator = new ProjectCommunicator(spaceName, apiKey);
             var projects = projectCommunicator.GetProjectList().ToList();
             foreach (var project in projects)
             {
                 Debug.WriteLine(project);
-                
+
                 // custom fields
                 var customFieldList = projectCommunicator.GetCustomFieldList(project.ProjectKey);
                 foreach (var customField in customFieldList)
@@ -31,31 +47,31 @@ namespace CSJSONBacklogSample
                 }
             }
 
-            // per project
-            foreach (var project in projects)
+            return projects;
+        }
+
+        private static void DumpIssues(IssueCommunicator issueCommunicator, Project project)
+        {
+            var count = issueCommunicator.GetIssuesCount(project.Id);
+
+            Debug.WriteLine(project + " " + count);
+
+            // issues in a project
+            var param = new QueryIssueParameters
             {
-                var issueCommunicator = new IssueCommunicator(spaceName, apiKey);
-                var count = issueCommunicator.GetIssuesCount(project.Id);
-
-                Debug.WriteLine(project + " " + count);
-
-                // issues in a project
-                var param = new QueryIssueParameters
+                ProjectIds = new List<int> { project.Id },
+                ParentChild = ParentChild.All,
+                Offset = 0,
+                Count = 100,// per 100 max
+                Order = Order.Asc,
+                Sort = Sort.Created
+            };
+            for (param.Offset = 0; param.Offset < count; param.Offset += 100)
+            {
+                var issues = issueCommunicator.GetIssues(param).ToList();
+                foreach (var issue in issues)
                 {
-                    ProjectIds = new List<int> { project.Id },
-                    ParentChild = ParentChild.All,
-                    Offset = 0,
-                    Count = 100,// per 100 max
-                    Order = Order.Asc,
-                    Sort = Sort.Created
-                };
-                for (param.Offset = 0; param.Offset < count; param.Offset += 100)
-                {
-                    var issues = issueCommunicator.GetIssues(param).ToList();
-                    foreach (var issue in issues)
-                    {
-                        Debug.WriteLine(issue);
-                    }
+                    Debug.WriteLine(issue);
                 }
             }
         }
