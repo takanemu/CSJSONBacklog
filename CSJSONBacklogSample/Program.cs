81bcc817149adb75afbaa17d2687e622a2a1e135
mtaniuchi <mtaniuchi@gmail.com>
2014-12-10 11:56:18 +0900

    [modified]SampleOfGetInfo

--- CSJSONBacklogSample/Program.cs
+++ CSJSONBacklogSample/Program.cs
@@ -23,20 +23,37 @@ namespace CSJSONBacklogSample
             string spaceName = Properties.Settings.Default.SpaceName;// must change!
             string apiKey = Properties.Settings.Default.APIKey;// must change!
 
-#if true
-            // Dump all project in space
-            var projects = DumpProjects(spaceName, apiKey).ToList();
+            // 1.get information
+            SampleOfGetInfo(spaceName, apiKey);
 
-            // Dump issues in a project
-            var issueCommunicator = new IssueCommunicator(spaceName, apiKey);
-            foreach (var project in projects)
+            // 2.update information
+            //SampleOfUpdateIssue(spaceName, apiKey, "PROJ1");
+        }
+
+
+
+        private static void SampleOfGetInfo(string spaceName, string apiKey)
+        {
+            var getInfoSample = new GetInfoSample(spaceName, apiKey);
+
+            var projects = getInfoSample.GetProjects().ToList();
+            getInfoSample.PrintProjectDetails(projects);
+
+            foreach (var project in projects.Where(x => x.ProjectKey.Equals("SND")))
             {
-                DumpIssues(issueCommunicator, project);
+                var issues = getInfoSample.GetIssues(project);
+                getInfoSample.PrintIssues(issues);
             }
-#else
+        }
+
+
+        #region UPDATE
+
+        private static void SampleOfUpdateIssue(string spaceName, string apiKey, string projectKey)
+        {
             var projectCommunicator = new ProjectCommunicator(spaceName, apiKey);
             var projects = projectCommunicator.GetProjectList().ToList();
-            var proj = projects.FirstOrDefault(x => x.ProjectKey.Equals("SND"));
+            var proj = projects.FirstOrDefault(x => x.ProjectKey.Equals(projectKey));
 
             var issueCommunicator = new IssueCommunicator(spaceName, apiKey);
             var count = issueCommunicator.GetIssuesCount(proj.Id);
@@ -57,89 +74,9 @@ namespace CSJSONBacklogSample
 
             issue.description += " Update by api @" + DateTime.Now;
             issueCommunicator.UpdateIssue(issue);
-#endif
         }
 
-        /// <summary>
-        /// Print all project info in space
-        /// </summary>
-        private static IEnumerable<Project> DumpProjects(string spaceName, string apiKey)
-        {
-            // project list
-            var projectCommunicator = new ProjectCommunicator(spaceName, apiKey);
-            var projects = projectCommunicator.GetProjectList().ToList();
-            foreach (var project in projects)
-            {
-                Debug.WriteLine(project);
+        #endregion
 
-                // disk usage
-                var usage = projectCommunicator.GetProjectDiskUsage(project.ProjectKey);
-                Debug.WriteLine("\t" + usage);
-
-                // project user
-                var users = projectCommunicator.GetProjectUserList(project.ProjectKey);
-                foreach (var user in users)
-                {
-                    Debug.WriteLine("\t" + user);
-                }
-
-                var admins = projectCommunicator.GetProjectAdministratorList(project.ProjectKey);
-                foreach (var user in admins)
-                {
-                    Debug.WriteLine("\t" + user);
-                }
-
-                // issue types
-                var issueTypes = projectCommunicator.GetIssueTypeList(project.ProjectKey);
-                foreach (var issueType in issueTypes)
-                {
-                    Debug.WriteLine("\t" + issueType);
-                }
-
-                // custom fields
-                var customFieldList = projectCommunicator.GetCustomFieldList(project.ProjectKey);
-                foreach (var customField in customFieldList)
-                {
-                    Debug.WriteLine("\t" + customField);
-                }
-
-#if false // TODO:{"errors":[{"message":"Authentication failure.","code":11,"moreInfo":""}]}
-                // git repositories
-                var repos = projectCommunicator.GetListofGitRepositories(project.ProjectKey);
-                foreach (var gitRepository in repos)
-                {
-                    Debug.WriteLine("\t" + gitRepository);
-                }
-#endif
-            }
-
-            return projects;
-        }
-
-        private static void DumpIssues(IssueCommunicator issueCommunicator, Project project)
-        {
-            var count = issueCommunicator.GetIssuesCount(project.Id);
-
-            Debug.WriteLine("\t" + project + " " + count);
-
-            // issues in a project
-            var param = new QueryIssueParameters
-            {
-                ProjectIds = new List<int> { project.Id },
-                ParentChild = ParentChild.All,
-                Offset = 0,
-                Count = 100,// per 100 max
-                Order = Order.Asc,
-                Sort = Sort.Created
-            };
-            for (param.Offset = 0; param.Offset < count; param.Offset += 100)
-            {
-                var issues = issueCommunicator.GetIssues(param).ToList();
-                foreach (var issue in issues)
-                {
-                    Debug.WriteLine("\t" + issue);
-                }
-            }
-        }
     }
 }
