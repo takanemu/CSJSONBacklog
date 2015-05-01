ddcdd32659bdbb2515010ce302f6f3f350266e87
mtaniuchi <mtaniuchi@gmail.com>
2015-05-01 18:39:57 +0900

    #8 [modified]Get User List #8

--- CSJSONBacklog.API/ISpaceAPI.cs
+++ CSJSONBacklog.API/ISpaceAPI.cs
@@ -1,8 +1,12 @@
 ﻿/* See the file "LICENSE" for the full license governing this code. */
 
+using System.Collections.Generic;
+using CSJSONBacklog.Model.Space;
+
 namespace CSJSONBacklog.API
 {
     public interface ISpaceAPI
     {
+        IEnumerable<User> GetUserList();
     }
 }
--- CSJSONBacklog.Communicator/SpaceCommunicator.cs
+++ CSJSONBacklog.Communicator/SpaceCommunicator.cs
@@ -1,14 +1,24 @@
 ﻿/* See the file "LICENSE" for the full license governing this code. */
 
+using System.Collections.Generic;
 using CSJSONBacklog.API;
+using CSJSONBacklog.Model.Space;
 
 namespace CSJSONBacklog.Communicator
 {
     public class SpaceCommunicator : AbstractCommunicator, ISpaceAPI
     {
-        public SpaceCommunicator(string spacename, string apiKey) : base(spacename, apiKey)
-        {
-        }
+        public SpaceCommunicator(string spacename, string apiKey)
+            : base(spacename, apiKey)
+        {}
 
+        /// <summary>
+        /// Returns list of users in your space.
+        /// </summary>
+        /// <see cref="http://developer.nulab-inc.com/docs/backlog/api/2/get-users"/>
+        public IEnumerable<User> GetUserList()
+        {
+            return GetT<IEnumerable<User>>(string.Format("https://{0}.backlog.jp/api/v2/users?apiKey={1}", Spacename, ApiKey));
+        }
     }
 }
