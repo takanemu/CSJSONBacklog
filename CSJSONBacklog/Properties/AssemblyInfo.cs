e5b149a9d2ff6869060dc7d99aaaf6f4992c3b9e
mtaniuchi <mtaniuchi@gmail.com>
2015-06-19 13:45:38 +0900

    add nupkg script.

--- CSJSONBacklog/Properties/AssemblyInfo.cs
+++ CSJSONBacklog/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // アセンブリに関連付けられている情報を変更するには、
 // これらの属性値を変更してください。
 [assembly: AssemblyTitle("CSJSONBacklog")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Backlog(www.backlog.jp) APIv2 library.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("mtaniuchi")]
 [assembly: AssemblyProduct("CSJSONBacklog")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright © mtaniuchi 2014-")]
+[assembly: AssemblyTrademark("CSJSONBacklog")]
 [assembly: AssemblyCulture("")]
 
 // ComVisible を false に設定すると、その型はこのアセンブリ内で COM コンポーネントから 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
